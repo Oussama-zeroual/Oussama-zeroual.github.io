d32bcfa3494ccb2d05dea9bde9394179d4190f15
samzer05 <oussamazeroual1000@gmail.com>
2026-03-24 15:04:56 +0100

    ,,,,,,,,,,,

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-<<<<<<< HEAD
+ HEAD
 document.querySelectorAll('.sidebar a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href'))
             .scrollIntoView({ behavior: 'smooth' });
     });
-=======
 document.querySelectorAll('.sidebar a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href'))
             .scrollIntoView({ behavior: 'smooth' });
     });
->>>>>>> ecdb63b (edited)
+ ecdb63b (edited)
 });
